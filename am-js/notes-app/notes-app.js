64b35d34b0573f17d24e2c79ca03af61de8dde75
Brandon Hexsel <brandon.hexsel@gmail.com>
2020-09-17 17:47:40 -0700

    'Starting DOM Manipulation'

---
+++ am-js/notes-app/notes-app.js
@@ -0,0 +1,42 @@
+// DOM - Document Object Model
+const notes = [
+  {
+    title: "My next trip",
+    body: "I would like to go to Spain",
+  },
+  {
+    title: "Habits to work on",
+    body: "Exercise. Eating a bit better.",
+  },
+  {
+    title: "Office modification",
+    body: "Get a new seat",
+  },
+  {
+    title: "Dream trip",
+    body: "I would like to go to Japan",
+  },
+];
+
+document.querySelector("button").addEventListener("click", function (e) {
+  console.log(e);
+  e.target.textContent = "The button was clicked";
+});
+
+// Query and remove
+// const p = document.querySelector("p");
+// console.log(p);
+// p.remove();
+
+// Query all and remove
+// const ps = document.querySelectorAll("p");
+// ps.forEach(function (p) {
+//   p.textContent = "******";
+//   // console.log(p.textContent);
+//   // p.remove();
+// });
+
+// Adding a new element
+// const newParagraph = document.createElement("p");
+// newParagraph.textContent = "This is a new paragraph from JavaScript";
+// document.querySelector("body").appendChild(newParagraph);
---
+++ am-js/todo-app/todo-app.js
@@ -0,0 +1,47 @@
+const todos = [
+  {
+    text: "buy food",
+    completed: true,
+  },
+  {
+    text: "do hw",
+    completed: false,
+  },
+  {
+    text: "watch nfl",
+    completed: true,
+  },
+  {
+    text: "practice js",
+    completed: false,
+  },
+];
+
+document.querySelector("button").addEventListener("click", function (e) {
+  console.log("I am adding a todo");
+});
+
+// You have 2 todos left (p element);
+const incompleteTodos = todos.filter(function (todo) {
+  return !todo.completed;
+});
+
+let summary = document.createElement("p");
+summary.textContent = `You have ${incompleteTodos.length} todos left`;
+document.querySelector("body").appendChild(summary);
+
+// Add a p for each todo above (use text value)
+todos.forEach(function (todo) {
+  let p = document.createElement("p");
+  p.textContent = todo.text;
+  document.querySelector("body").appendChild(p);
+});
+
+// // Removing an element from a list of paragraphs if it includes "the"
+// const paragraphs = document.querySelectorAll("p");
+
+// paragraphs.forEach(function (p) {
+//   if (p.textContent.includes("the")) {
+//     p.remove();
+//   }
+// });
