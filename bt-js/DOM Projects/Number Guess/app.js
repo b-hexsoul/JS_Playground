07261e5f02dd39a10a5f9a936431dc91b4886094
Brandon Hexsel <brandon.hexsel@gmail.com>
2020-09-24 02:47:01 -0700

    'Add finished number guesser'

--- bt-js/DOM Projects/Number Guess/app.js
+++ bt-js/DOM Projects/Number Guess/app.js
@@ -10,7 +10,7 @@ GAME FUNCTION:
 // Game values
 let min = 1,
   max = 10,
-  winningNum = 2,
+  winningNum = getRandomNum(),
   guessesLeft = 3;
 
 // UI Elements
@@ -25,25 +25,63 @@ const gameEl = document.querySelector("#game"),
 minNum.textContent = min;
 maxNum.textContent = max;
 
+// Play Again Event Listener
+gameEl.addEventListener("mousedown", function (e) {
+  if (e.target.className === "play-again") {
+    window.location.reload();
+  }
+});
+
 // Event Listener
 guessBtn.addEventListener("click", function () {
   let guess = parseInt(guessInput.value);
+  console.log(guess);
+  console.log(max);
+  console.log(guess > max);
 
   // Validate Input - check not blank, since parseInt (blank will return NaN), not less than min or more than max
   if (isNaN(guess) || guess < min || guess > max) {
     setMessage(`Please enter a number between ${min} and ${max}`, "red");
+  } else {
+    if (guess === winningNum) {
+      // Game Over - loss
+      gameOver(true, `${winningNum} is correct! YOU WIN!`);
+    } else {
+      // Wrong Number
+      guessesLeft--;
+      if (guessesLeft === 0) {
+        // Game Over - Loss
+        gameOver(false, `The winning number is ${winningNum}! You lose!`);
+      } else {
+        // Game continues - answer wrong
+        guessInput.style.borderColor = "red";
+        setMessage(`Try again, wrong number. You have ${guessesLeft} guesses left.`, "black");
+        guessInput.value = "";
+      }
+    }
   }
 
   //Check if won
-  if (guess === winningNum) {
-    //disable Input
-    guessInput.disabled = true;
-    guessInput.style.borderColor = "green";
-    setMessage(`${winningNum} is correct! YOU WIN!`, "green");
-  }
 });
 
 function setMessage(msg, color) {
   messageEl.textContent = msg;
   messageEl.style.color = color;
+}
+
+function gameOver(won, msg) {
+  let color;
+  won === true ? (color = "green") : (color = "red");
+  //disable Input
+  guessInput.disabled = true;
+  guessInput.style.borderColor = color;
+  setMessage(msg, color);
+
+  // Play Again?
+  guessBtn.value = "Play Again";
+  guessBtn.className += "play-again";
+}
+
+function getRandomNum() {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
